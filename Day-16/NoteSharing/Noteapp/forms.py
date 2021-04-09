ece4a01ce220ab2f3e1bc0322c7607265c350b1d
APSSDC <rravikumar34@gmail.com>
2021-04-09 16:50:35 +0530

    Day-16 Updated

---
+++ Day-16/NoteSharing/Noteapp/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class UsForm(UserCreationForm):
+	password1=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control","placeholder":"password"}))
+	password2=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control","placeholder":"confirm password"}))
+	class Meta:
+		model=User
+		fields=['username']
+		widgets={
+		"username":forms.TextInput(attrs={
+			"class":"form-control",
+			"placeholder":"Username",
+			}),
+
+		}
+
+		
